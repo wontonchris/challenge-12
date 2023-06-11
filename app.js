8c4b5e6897039d056fc02bd9cb31c6f4be3f1e9c
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-06-11 12:51:40 -0500

    questions files

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Dependencies
 require('dotenv').config();
 const { buildConnectionOptions, createConnection } = require('./config/dbConfig');
+const inquirer = require('inquirer'); 
+const questions = require('./lib/questions');
 
 const toLog = `
 EMPLOYEE TRACKER
@@ -17,6 +19,13 @@ EMPLOYEE TRACKER
 
 async function main() {
     const connection = await createConnection(buildConnectionOptions());
+
+// ask the user what they want to do
+const {menuOption} = await inquirer.prompt(questions[0]);
+
+    const {createPrompt} = inquirer;
+    const prompt = createPrompt();
+ }
   
 
     const [departments] = await connection.execute('SELECT * FROM departments', []);
